6dc80d721f9e1e85cabccd7159b2fd1dd4de5ece
Harshh10 <harshvardhan.p1006@gmail.com>
2026-03-03 10:29:37 +0530

    Create tableOf2.c

---
+++ conditional/tableOf2.c
@@ -0,0 +1,15 @@
+//print table of 2
+
+#include<stdio.h>
+
+int main(){
+   
+    int num = 2;
+    int a = 1;
+    do
+    {
+       printf("%d x %d = %d\n",num,a,num*a);
+       a++;
+    } while (a<=10);
+    
+}

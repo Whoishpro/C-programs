de4ccb9186ad4c2cee5a3e71867cc081f305d1db
Harshh10 <harshvardhan.p1006@gmail.com>
2026-03-05 23:46:37 +0530

    Create SumofFirst10NaturalNos.c

---
+++ loops/SumofFirst10NaturalNos.c
@@ -0,0 +1,15 @@
+// sum of first 10 natural nos
+#include<stdio.h>
+
+int main(){
+
+    int i=1;
+    int sum=0;
+     printf("Sum of first 10 natural nos: \n");
+    do
+    {
+        printf("%d\n",sum+=i);
+        i++;
+    } while (i<=10);
+
+}

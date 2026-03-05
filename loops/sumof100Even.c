8531c6244ffebf358508f97f6fab970590e28a6a
Harshh10 <harshvardhan.p1006@gmail.com>
2026-03-05 23:48:37 +0530

    Create sumof100Even.c

---
+++ loops/sumof100Even.c
@@ -0,0 +1,15 @@
+//sum of first 100 even nos
+#include<stdio.h>
+int main(){
+    int i=1;
+    int sum=0;
+    do
+    {
+        if (i%2==0)
+        {
+           printf("%d\n",sum+=i);
+        }
+        i++;
+        
+    } while (i<=100);
+}   
